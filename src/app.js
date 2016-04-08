7f9e0b73ed997d2bc47a599a8d9f09531246dd23
Adeury09 <Adeury0909>
2016-04-08 02:14:57 -0300

    Primera parte del juego, con las imagenes relacionada y fondo

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 
 var HelloWorldLayer = cc.Layer.extend({
-    sprite:null,
+    fondo:null,
+    redBird:null,
+    blueBird:null,
+    impulsor:null,
+    
     ctor:function () {
         //Este es el proyecto final, aqui tendrán que hacer todo desde cero
         this._super();
@@ -8,12 +12,28 @@ var HelloWorldLayer = cc.Layer.extend({
 
         var helloLabel = new cc.LabelTTF("Angry INTEC", "Arial", 38);
         helloLabel.setPosition(size.width / 2, size.height / 2 + 200);
-        this.addChild(helloLabel, 5);
+        this.addChild(helloLabel, 1);
 
-        this.sprite = new cc.Sprite(res.HelloWorld_png);
-        this.sprite.setPosition(size.width / 2, size.height / 2);
-        this.addChild(this.sprite, 0);
-
+        this.fondo = new cc.Sprite(res.Fondo3_jpg);
+        this.fondo.setPosition(size.width / 2, size.height / 2);
+        this.fondo.setScale(0.45,0.45);
+        this.addChild(this.fondo, 0);
+        
+        this.redBird = new cc.Sprite(res.RedBird_png);
+        this.redBird.setPosition(150,105);
+        this.redBird.setScale(0.2,0.2);
+        this.addChild(this.redBird, 1);
+        
+        this.blueBird = new cc.Sprite(res.BLueBird_png);
+        this.blueBird.setPosition(110,105);
+        this.blueBird.setScale(0.2,0.2);
+        this.addChild(this.blueBird, 1);
+        
+        this.impulsor = new cc.Sprite(res.impulsor_png);
+        this.impulsor.setPosition(210,135);
+        this.impulsor.setScale(0.5,0.5);
+        this.addChild(this.impulsor,1);
+        
         return true;
     }
 });
--- src/resource.js
+++ src/resource.js
@@ -1,5 +1,12 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
+    Fondo3_jpg: "res/Fondo3.jpg",
+    RedBird_png: "res/RedBird.png",
+    BLueBird_png: "res/BlueBird.png",
+    GramdRedBird_png: "res/GramdRedBird.png",
+    YellowBird_png: "res/YellowBird.png",
+    impulsor_png: "res/Impulsor.png",
+    
 };
 
 var g_resources = [];
